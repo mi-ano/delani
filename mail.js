8eaf2a0231d58cec779a28b4f02a4da606d08e28
mi-ano <ryanmiano68@gmail.com>
2019-06-10 09:09:19 +0300

    added mail.js

---
+++ mail.js
@@ -0,0 +1,6 @@
+function mail(){
+    var Name = document.getElementById('name').value;
+    var Email = document.getElementById('email').value;
+    var Message = document.getElementById('message').value;
+    alert(Name + " of email "+ Email + " we have recieved your message. Thank you for contacting us");
+}
